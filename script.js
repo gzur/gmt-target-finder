d1bd99ca2f2b50b4fda44c531d3e412088069b23
Gissur Þórhallsson <gzzzur@gmail.com>
2021-10-07 16:50:53 +0000

    Formatting

--- script.js
+++ script.js
@@ -18,23 +18,23 @@
 
 //DETERMINE CO_ORDINATES
 
-let blocksXY=[];
-let blocks=document.getElementsByName('v');
-let Xsum=0;
-let Ysum=0;
+let blocksXY = [];
+let blocks = document.getElementsByName('v');
+let Xsum = 0;
+let Ysum = 0;
 
-for (var i=0; i<blocks.length; i++){
-    blocksXY[i]=blocks[i].value.match(/(\d\d?\d?)-(\d\d?\d?)/);
-    Xsum+=parseInt(blocksXY[i][1]);
-    Ysum+=parseInt(blocksXY[i][2]);
+for (var i = 0; i < blocks.length; i++) {
+    blocksXY[i] = blocks[i].value.match(/(\d\d?\d?)-(\d\d?\d?)/);
+    Xsum += parseInt(blocksXY[i][1]);
+    Ysum += parseInt(blocksXY[i][2]);
 }
 
-let xLoc=parseInt(Xsum/blocksXY.length);
-let yLoc=parseInt(Ysum/blocksXY.length);
-if(xLoc<1){xLoc=0;}
-if(yLoc<1){yLoc=0;}
-if(xLoc>98 && xLoc<99){xLoc=99;}
-if(yLoc>98 && yLoc<99){yLoc=99;}
+let xLoc = parseInt(Xsum / blocksXY.length);
+let yLoc = parseInt(Ysum / blocksXY.length);
+if (xLoc < 1) { xLoc = 0; }
+if (yLoc < 1) { yLoc = 0; }
+if (xLoc > 98 && xLoc < 99) { xLoc = 99; }
+if (yLoc > 98 && yLoc < 99) { yLoc = 99; }
 
 if (window.location.href.match(/GmtbcTarget=\d+,\d+/)) {
     var results = window.location.href.match(/GmtbcTarget=(\d+),(\d+)/);
@@ -44,35 +44,35 @@ if (window.location.href.match(/GmtbcTarget=\d+,\d+/)) {
 }
 
 // If there are cookies, and in particular our cookies
-if ((document.cookie.length>0) && (document.cookie.indexOf("GmtX=") != -1) && (document.cookie.indexOf("GmtX=") != -1)) {
+if ((document.cookie.length > 0) && (document.cookie.indexOf("GmtX=") != -1) && (document.cookie.indexOf("GmtX=") != -1)) {
 
-    let c_start=document.cookie.indexOf("GmtX=");
-    c_start+=5;
-    let c_end=document.cookie.indexOf(";",c_start);
-    if (c_end==-1) c_end=document.cookie.length;
-    var xTarget = document.cookie.substring(c_start,c_end);
+    let c_start = document.cookie.indexOf("GmtX=");
+    c_start += 5;
+    let c_end = document.cookie.indexOf(";", c_start);
+    if (c_end == -1) c_end = document.cookie.length;
+    var xTarget = document.cookie.substring(c_start, c_end);
 
-    c_start=document.cookie.indexOf("GmtY=");
-    c_start+=5;
-    c_end=document.cookie.indexOf(";",c_start);
-    if (c_end==-1) c_end=document.cookie.length;
-    var yTarget = document.cookie.substring(c_start,c_end);
+    c_start = document.cookie.indexOf("GmtY=");
+    c_start += 5;
+    c_end = document.cookie.indexOf(";", c_start);
+    if (c_end == -1) c_end = document.cookie.length;
+    var yTarget = document.cookie.substring(c_start, c_end);
 
     // Compare Loc and target, and determine direction
     var gmt_rows_obj = document.getElementsByClassName("cp")[0].children[0].children[0].rows;
-    var gmt_arr_rows = [gmt_rows_obj[1],gmt_rows_obj[2],gmt_rows_obj[3]];
+    var gmt_arr_rows = [gmt_rows_obj[1], gmt_rows_obj[2], gmt_rows_obj[3]];
     var gmt_dirs = {
-        NW : gmt_arr_rows[0].children[0],
-        N :  gmt_arr_rows[0].children[1],
-        NE : gmt_arr_rows[0].children[2],
+        NW: gmt_arr_rows[0].children[0],
+        N: gmt_arr_rows[0].children[1],
+        NE: gmt_arr_rows[0].children[2],
 
-        W :    gmt_arr_rows[1].children[0],
-        HERE : gmt_arr_rows[1].children[1],
-        E :    gmt_arr_rows[1].children[2],
+        W: gmt_arr_rows[1].children[0],
+        HERE: gmt_arr_rows[1].children[1],
+        E: gmt_arr_rows[1].children[2],
 
-        SW : gmt_arr_rows[2].children[0],
-        S :  gmt_arr_rows[2].children[1],
-        SE : gmt_arr_rows[2].children[2]
+        SW: gmt_arr_rows[2].children[0],
+        S: gmt_arr_rows[2].children[1],
+        SE: gmt_arr_rows[2].children[2]
     }
 
     var gmtWhichWay;
@@ -94,10 +94,10 @@ if ((document.cookie.length>0) && (document.cookie.indexOf("GmtX=") != -1) && (d
         gmtWhichWay = gmt_dirs.W;
     } else {
         // at target
-        gmtWhichWay= gmt_dirs.HERE;
+        gmtWhichWay = gmt_dirs.HERE;
     }
     gmtWhichWay.style.border = "5px solid red";
     let yDist = Math.abs(yTarget - yLoc)
     let xDist = Math.abs(xTarget - xLoc)
-    document.getElementsByClassName("gthome")[0].innerHTML += "<p>Location: "+xLoc+","+yLoc+" Distance:"+Math.max(xDist, yDist)+"</p>";
+    document.getElementsByClassName("gthome")[0].innerHTML += "<p>Location: " + xLoc + "," + yLoc + " Distance:" + Math.max(xDist, yDist) + "</p>";
 }
